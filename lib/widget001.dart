160f41c523fdab7c91ab9c26a160df303db451fc
BHUVANESHWARAN K <bhuvaneshw522@gmail.com>
2024-10-05 07:40:14 +0530

    widget001

---
+++ lib/widget001.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Widget001 extends StatefulWidget {
+  const Widget001({super.key});
+
+  @override
+  State<Widget001> createState() => _Widget001State();
+}
+
+class _Widget001State extends State<Widget001> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              showDialog(
+                  context: context,
+                  builder: (context) => AboutDialog(
+                        applicationIcon: FlutterLogo(),
+                        applicationLegalese: 'Legalese',
+                        applicationName: 'Flutter Widgets',
+                        applicationVersion: 'V1.0',
+                        children: [Text("this is About Dialog!..")],
+                      ));
+            });
+          },
+          child: const Text("Show About Dialog!")),
+    );
+  }
+}
